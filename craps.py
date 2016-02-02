5a309d1935a6b0f821ff650d6d108af8db5063c3
Rishabh <rishabhshah28@gmail.com>
2016-02-02 10:21:00 -0500

    Finished craps.py

--- craps.py
+++ craps.py
@@ -5,66 +5,143 @@
 #imports
 import random
 import sys
+import textwrap
 
 #global variables
 diceValues = list(xrange(1,7))
 
 #Defining functions
+totalValue = 0
+
 def rollDice():
 	diceValue1 = str(random.choice(diceValues))
 	diceValue2 = str(random.choice(diceValues))
+	global totalValue
 	totalValue = str(int(diceValue2)+int(diceValue1))
-	print "Dice 1 is " + diceValue1 + "."
-	print "Dice 2 is " + diceValue2 + "."
-	print "Total value of dice is " + totalValue + "."
+	print textwrap.fill("Dice 1 is " + diceValue1 + ".")
+	print textwrap.fill("Dice 2 is " + diceValue2 + ".")
+	print textwrap.fill("Total value of dice is " + totalValue + ".")
 
 def playAnotherTime():
 	while True:
 		playAgain = raw_input("Would you like to play again? (yes/no): ").lower()
 		if playAgain == "no" or playAgain == "n":
 			print ""
-			print "Goodbye"
+			print textwrap.fill("Goodbye")
 			sys.exit()
 		if playAgain == "yes" or playAgain == "y":
 			print ""
 			return
 		if playAgain != "yes" or playAgain != "y" or playAgain != "no" or playAgain != "n":
 			print ""
-			print "You entered something that does not compute. Please try again"
+			print textwrap.fill("You entered something that does not compute. Please try again")
 
-def play():
+def start():
 	while True:
-		play = raw_input("Would you like to play? (yes/no): ").lower()
-		if play == "no" or playAgain == "n":
-			print ""
-			print "Goodbye"
-			sys.exit()
-		if play == "yes" or playAgain == "y":
-			print ""
-			return
-		if play != "yes" or playAgain != "y" or playAgain != "no" or playAgain != "n":
-			print ""
-			print "You entered something that does not compute. Please try again"
+		global ifcontinue
+		ifcontinue = raw_input("Would you like to play craps? (yes/no): ")
+		if ifcontinue == "yes" or \
+		   ifcontinue == "y":
+			break
+		if ifcontinue == "no" or \
+		   ifcontinue == "n":
+			break
+		if ifcontinue != "yes" or \
+		   ifcontinue != "y" or \
+		   ifcontinue != "no" or \
+		   ifcontinue != "n":
+			print textwrap.fill("You entered something that does compute. Please try again.")
 
-def main():
-	print "Welcome to Rishabh Shah's craps game!"
-	play()
-	rollDice()
-	if int(totalValue) == 7 or int(totalValue) == 11:
-		print "You win!"
-		playAnotherTime()
-	if int(totalValue) == 2 or int(totalValue) == 3 or int(totalValue) == 12:
-		print "You lose!"
-		playAnotherTime
-	if int(totalValue) != 7 or int(totalValue) != 11 or int(totalValue) != 2 or int(totalValue) != 3 or int(totalValue) != 12:
-		point = totalValue
-		print point + " is the point!"
-		rollDice()
-		if point == totalValue:
-			print "You win!"
-			playAnotherTime
-		elif int(point) == 7 or int(point) == 11:
-			print "You lose!"
-			playAnotherTime
-		elif int(point) != int(totalValue) or int(point) != 7 or int(point) != 11:
+def game():
+	global ifcontinue
+	print ""
+	start()
+	if ifcontinue == "no" or \
+	   ifcontinue == "n":
+	   	print textwrap.fill("Goodbye.")
+		sys.exit()
+		
+	while True:
+		while True:
+			print ""
 			rollDice()
+			print ""
+
+			if totalValue == '7' or \
+			   totalValue == '11':
+				print textwrap.fill("You win.")
+				gameState = 0
+			elif totalValue == '2' or \
+			   totalValue == '3' or \
+			   totalValue == '12':
+				print textwrap.fill("You lost.")
+				gameState = 1
+			else:
+				print textwrap.fill("The point has been established. It is " + totalValue + ".")
+				point = totalValue
+				gameState = 2
+			if gameState == 1:
+				break
+			elif gameState == 0:
+				break
+
+			while True:
+				ifcontinue = raw_input("Would you like to roll for the point? (yes/no): ")
+				if ifcontinue == "yes" or \
+				   ifcontinue == "y":
+					break
+				if ifcontinue == "no" or \
+				   ifcontinue == "n":
+					break
+				if ifcontinue != "yes" or \
+				   ifcontinue != "y" or \
+				   ifcontinue != "no" or \
+				   ifcontinue != "n":
+					print textwrap.fill("You entered something that does compute. Please try again.")
+			if ifcontinue == "no" or \
+			   ifcontinue == "n":
+				break
+			
+			print ""
+			while True:
+				print textwrap.fill("Rolling...")
+				print ""
+				rollDice()
+				print ""
+				if totalValue == point:
+					print textwrap.fill("You won.")
+					gameState = 0
+				elif totalValue == 7:
+					print textwrap.fill("You lose.")
+					gameState = 1
+				else:
+					print textwrap.fill("You will have to roll again because the point is " + point + " and that does not equal " + totalValue + ".")
+					gameState == 2
+				if gameState == 0:
+					break
+				if gameState == 1:
+					break
+
+				while True:
+					ifcontinue = raw_input("Would you like to roll again? (yes/no): ")
+					if ifcontinue == "yes" or \
+					   ifcontinue == "y":
+						break
+					if ifcontinue == "no" or \
+					   ifcontinue == "n":
+						break
+					if ifcontinue != "yes" or \
+					   ifcontinue != "y" or \
+					   ifcontinue != "no" or \
+					   ifcontinue != "n":
+						print textwrap.fill("You entered something that does compute. Please try again.")
+				if ifcontinue == "no" or \
+				   ifcontinue == "n":
+					break
+
+			break
+		playAnotherTime()
+
+
+print textwrap.fill("Welcome to Rishabh Shah's craps game!")
+game()
